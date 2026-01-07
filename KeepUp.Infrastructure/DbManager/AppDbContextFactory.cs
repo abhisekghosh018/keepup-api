47c8619f506853bde34015781ad02b4c1d7ec3bf
abhisekghosh018 <abhisekghosh018@gmail.com>
2026-01-07 17:43:16 +0530

    remove dbcontext factory

--- KeepUp.Infrastructure/DbManager/AppDbContextFactory.cs
+++ KeepUp.Infrastructure/DbManager/AppDbContextFactory.cs
@@ -1,33 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+//using Microsoft.Extensions.Configuration;
 
-namespace KeepUp.Infrastructure.DbManager
-{
-    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
-    {
-        public AppDbContext CreateDbContext(string[] args)
-        {
-            // Build configuration ONLY from environment variables
-            // (works in Docker, CI, local)
-            var configuration = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .Build();
+//namespace KeepUp.Infrastructure.DbManager
+//{
+//    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+//    {
+//        public AppDbContext CreateDbContext(string[] args)
+//        {
+//            // Build configuration ONLY from environment variables
+//            // (works in Docker, CI, local)
+//            var configuration = new ConfigurationBuilder()
+//                .AddEnvironmentVariables()
+//                .Build();
 
-            // Prefer standard ConnectionStrings, fallback to env-style
-            var connectionString =
-                configuration.GetConnectionString("DefaultConnection")
-                ?? configuration["ConnectionStrings__DefaultConnection"];
+//            // Prefer standard ConnectionStrings, fallback to env-style
+//            var connectionString =
+//                configuration.GetConnectionString("DefaultConnection")
+//                ?? configuration["ConnectionStrings__DefaultConnection"];
 
-            if (string.IsNullOrWhiteSpace(connectionString))
-                throw new InvalidOperationException(
-                    "Design-time connection string 'DefaultConnection' is missing.");
+//            if (string.IsNullOrWhiteSpace(connectionString))
+//                throw new InvalidOperationException(
+//                    "Design-time connection string 'DefaultConnection' is missing.");
 
-            var options = new DbContextOptionsBuilder<AppDbContext>()
-                .UseNpgsql(connectionString)
-                .Options;
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//                .UseNpgsql(connectionString)
+//                .Options;
 
-            return new AppDbContext(options);
-        }
-    }
-}
+//            return new AppDbContext(options);
+//        }
+//    }
+//}
